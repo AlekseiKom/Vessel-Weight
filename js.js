0fbac1d62932f4b387bedeec0b033eca59a36d7a
AlekseiKom <53940821+AlekseiKom@users.noreply.github.com>
2020-02-29 17:21:33 +0200

    Added veriables survey

--- js.js
+++ js.js
@@ -11,8 +11,9 @@ function calculate() {
 
 let resultWeight = document.getElementById("weight").value;
 let itemsSum = document.getElementById("totalUnits").value;
-
 let liftSum = document.getElementById("lift").value; 
+let surWeight1 = document.getElementById("surv1").value; 
+let surWeight2 = document.getElementById("surv2").value; 
 
 
 //вывод данных
