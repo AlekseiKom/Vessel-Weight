a874da14c4b78b84db8ddb417dee234e2a01f76c
AlekseiKom <53940821+AlekseiKom@users.noreply.github.com>
2020-02-29 17:45:04 +0200

    Verified ways

--- js.js
+++ js.js
@@ -14,9 +14,12 @@ let itemsSum = document.getElementById("totalUnits").value;
 let liftSum = document.getElementById("lift").value; 
 let surWeight1 = document.getElementById("surv1").value; 
 let surWeight2 = document.getElementById("surv2").value; 
+let resultField = document.getElementById("testResult"); 
 
 
 //вывод данных
+
+resultField.innerHTML(resultWeight);
 
 /*document.getElementsByClassName("header").innerHTML = ("<br/>" + "<br/>" + "Введенный общий вес (брутто): " + resultWeight + "кг.");
 document.getElementsByClassName("header").innerHTML = ("<br/>" + "<br/>" + "Общее кол-во связок: " + itemsSum + " шт."); */
