48360f3cb9ccabd0a3a110f2db64855477e829c0
AlekseiKom <53940821+AlekseiKom@users.noreply.github.com>
2020-05-29 16:13:48 +0300

    Output fixed, main values calculated.

--- newJS.js
+++ newJS.js
@@ -4,16 +4,26 @@
 
     let totalWeight = document.getElementById("weight").value; //Общий вес
     let itemsSum = document.getElementById("totalUnits").value;  //Общее кол-во св/шт
-    let liftSum = document.getElementById("lift").value;   //Ко-во в одном подъеме
+    let liftSumItems = document.getElementById("lift").value;   //Ко-во в одном подъеме
     let surWeight1 = document.getElementById("survei1").value; //Вес сюрв. 1
     let surWeight2 = document.getElementById("survei2").value; //Вес сюрв. 2
     let resultField = document.getElementById("testResult");  //Окно результата 
 
 
-     let oneItem = parseFloat(totalWeight/itemsSum);
-     resultField.innerHTML= "Вес одной св/шт. = " + oneItem;
-     
-     
+     let oneItemWeight = parseFloat(totalWeight/itemsSum);  //рассчет веса одной св./шт.
+    
+       function roundTo10(num) {      //округление до 10
+        return Math.round(num/10)*10;
+        }
+
+     liftSumWeight = roundTo10(liftSumItems*oneItemWeight); //расчет веса одного подъема
+
+     //Вывод основных данных
+     resultField.innerHTML= 
+     "Введенный общий вес (брутто) :  "  +  totalWeight + " кг." + "<br>" +
+     "Общее кол-во св/шт. :  "  +  itemsSum + " ." + "<br>" +
+     "Средний вес одной св/шт. :  " + oneItemWeight + " кг." + "<br>" +
+     "Средний вес одного подъема из " + liftSumItems + " св/шт." + " =  " + liftSumWeight + " кг."; 
 
     }
 
