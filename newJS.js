0e17dae5be4b4bd750d1a7f8c49b00f7e569e1cf
AlekseiKom <53940821+AlekseiKom@users.noreply.github.com>
2020-06-02 16:30:41 +0300

    workability tested

--- newJS.js
+++ newJS.js
@@ -22,18 +22,17 @@
      resultField.innerHTML= 
      "Введенный общий вес (брутто) :  "  +  totalWeight + " кг." + "<br>" +
      "Общее кол-во св/шт. :  "  +  itemsSum + " ." + "<br>" +
-     "Средний вес одной св/шт. :  " + oneItemWeight + " кг." + "<br>" +
+     "Средний вес одной св/шт. :  " + (oneItemWeight.toFixed(3)) + " кг." + "<br>" +
      "Средний вес одного подъема из " + liftSumItems + " св/шт." + " =  " + liftSumWeight + " кг."; 
      
       
-     //Расчет основной перевески
+     //               Р А С Ч Е Т    О С Н О В Н О Й    П Е Р Е В Е С К И 
       
       let totaLiftSum, lastLiftItems, lastLift;
 
       mainLiftsSumItems = liftSumItems*(Math.floor(itemsSum/liftSumItems)); //расчет кол-ва св./шт без последнего подъема 
 
       mainLifts = mainLiftsSumItems/liftSumItems;                  //расчет основного кол-ва подъемов
-
 
       lastLiftItems =  itemsSum - mainLiftsSumItems                //Расчет кол-ва в последнем подъеме
       if (lastLiftItems > 0) {
@@ -42,21 +41,41 @@
       else {
         lastLift = 0;
        }
-
+       
+      lastLiftWeight = roundTo10(lastLiftItems*oneItemWeight);     //Расчет веса в поледнего подъема
+      
       totaLiftSum = Math.floor(itemsSum/liftSumItems) + lastLift;  //Общее кол-во подъемов
+      
+      //Выбор случайного числа (разлет до 80 кг) при расчете погрешности по весу каждой св./шт  
+      
+      /*function getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+      }*/
+  
+      let randArr = [10, -10, 20 ,-20, 0, 30, -30, 40, -40]        
+      let rand = Math.floor(Math.random() * randArr.length);
+      let diff = randArr[rand];
 
-      let weightSum = [];
-      for (i=1; i<= mainLifts; i++) {
-        console.log(i)
-      } 
-        
+      console.log(diff);
+    
+      let weightSum = []; 
+
+      for (step = 0; step < mainLifts; step++) {
+        weightSum.push(liftSumWeight);
+      }
+      if (lastLift > 0) {
+      weightSum.push(lastLiftWeight);
+      }
+    
+      
       
       //тестовая проверка
       console.log('Основное число подъемов (кол-во) = ' + mainLifts + ' по ' + liftSumItems + ' св./шт')
       console.log('Кол-во в основных подъемах = ' + mainLiftsSumItems);
       console.log('Кол-во в последнем подъеме = ' + lastLiftItems);
       console.log('Общее кол-во подъемов = ' + totaLiftSum);
-      
+      console.log(weightSum);
+    
       
     
       
